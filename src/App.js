ed2d4757d685fbccfdfc08627ec441a3339f85e0
Majdi Hajjem <marwa25029133marwa@gmail.com>
2022-05-12 17:48:12 +0100

    check point jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import myimg from './imageInSrc.jpg';
 import './App.css';
-
+import './style.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+<div style={{border:"solid 1px black",maxWidth:"100vw"}}>
+
+<h1 className="title red">Majdi Hajjem</h1>
+
+<br />
+
+<img src={myimg} alt="image" />
+
+<br />
+
+<img src="/imageInPublic.jpg" alt="fleur"/>
+
+</div>
+
+<video width="320" height="240" controls>
+
+<source src='/gmc.mp4' type="video/mp4" ></source>
+
+</video>
+</>
   );
 }
 
